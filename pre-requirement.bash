900519ef35a291836d75a05bebf41669d747a885
Ryodo Tanaka <groadpg@gmail.com>
2018-10-31 12:22:06 +0900

    [fix] pre requiremnts

--- pre-requirement.bash
+++ pre-requirement.bash
@@ -2,11 +2,11 @@
 
 sudo apt install -y pkg-config libboost-serialization-dev libboost-filesystem-dev libboost-system-dev libboost-program-options-dev libboost-test-dev libeigen3-dev libode-dev
 sudo apt install software-properties-common
-sudo apt install -y cmake git libboost-system-dev libboost-thread-dev freeglut3 freeglut3-dev libglpk-dev python-dev python-opengl libxmu-dev libxi-dev libqt4-dev libassimp-dev ffmpeg g++-5 libboost1.55-all-dev libeigen3-dev libgtest-dev
+sudo apt install -y cmake git libboost-system-dev libboost-thread-dev freeglut3 freeglut3-dev libglpk-dev python-dev python-opengl libxmu-dev libxi-dev libqt4-dev libassimp-dev ffmpeg
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt update
 sudo apt -y upgrade
-sudo apt install -y g++-5 cmake pkg-config libboost1.55-serialization-dev libboost1.55-filesystem-dev libboost1.55-system-dev libboost1.55-program-options-dev libboost1.55-test-dev libeigen3-dev libode-dev
+sudo apt install -y g++-5 cmake pkg-config libboost1.55-serialization-dev libboost1.55-filesystem-dev libboost1.55-system-dev libboost1.55-program-options-dev libboost1.55-test-dev libeigen3-dev libode-dev libglpk-dev libgtest-dev
 export CXX=g++-5
 
 cd /usr/src/gtest
